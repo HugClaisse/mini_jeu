c4a2991b6825adf64f6ace8311078a0cca0a2cf9
HugClaisse <freehugh92@gmail.com>
2021-10-19 16:04:39 +0200

    fifth

--- mini_jeu_POO/lib/player.rb
+++ mini_jeu_POO/lib/player.rb
@@ -31,3 +31,65 @@ class Player
     end
 
 end
+
+class HumanPlayer < Player
+    attr_accessor :weapon_level, :name, :life_points
+
+    def initialize(name)
+        @name = name.to_s
+        @life_points = 100
+        @weapon_level = 1
+    end
+
+    def show_state
+        puts "#{name} a #{life_points} PV, son arme est de niveau #{weapon_level}"
+    end
+
+    def compute_damage
+        rand(1..6)*@weapon_level
+    end
+
+    def compute_damage
+        rand(1..6)*@weapon_level
+    end
+
+    def search_weapon
+        new_weapon = rand(1..6)
+        puts "Tu as trouvé une arme de niveau : #{new_weapon} !"
+
+        if new_weapon > @weapon_level
+            @weapon_level = new_weapon
+            puts "Yes ! Elle est mieux que ton arme actuelle, on l'équipe !!"
+        else
+            puts "€$*#... C'est arme est nulle ..."
+        end
+    end
+
+    def search_health_pack
+        new_heal = rand(1..6)
+        case new_heal
+        when 1
+            puts "Il n'y a rien ..."
+        when 2..5
+            puts "+50 PV !"
+            if @life_points <= 50
+                @life_points +=50
+                puts show_state
+            else
+                @life_points = 100
+                puts show_state
+            end
+        when 6
+            puts "wow !! +80 PV petit chanceux"
+            if @life_points <= 20
+                @life_points +=80
+                puts show_state
+            else
+                life_points = 100
+                puts show_state
+            end
+    end
+end
+end
+
+
